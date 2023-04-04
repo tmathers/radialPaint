67a9ab9d934832702e1753738938b3017c8a4a49
tmathers <tara.mathers@gmail.com>
2023-04-04 10:30:10 -0300

    Allow drawing with lines or paths

--- js/editor.js
+++ js/editor.js
@@ -10,8 +10,7 @@ var startY = 0;
 var mouseX = 0;
 var mouseY = 0;
 var isDrawing = false;
-var existingLines = [];
-var existingPenPaths = [];  
+var paths = [];  
 
 const ONE_THIRD_PI = 1.047198;
 const PI = 3.14159;
@@ -21,10 +20,10 @@ var reflect = true;
 var lineWidth = 2;
 
 
-var drawMode = 'pen';
 var mouseDown = false;
 
 function draw() {
+
   ctx.fillStyle = "#fff";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
   ctx.strokeStyle = "black";
@@ -40,30 +39,13 @@ function draw() {
   for (var j = 0; j < numPoints; j++) {
 
     ctx.translate( canvasWidth / 2, canvasHeight / 2 );
-    
     ctx.rotate(ROTATE * j);
-
     ctx.translate( -canvasWidth / 2, -canvasHeight / 2 );
-
-    ctx.beginPath();
-
-    for (var i = 0; i < existingLines.length; ++i) {
-      var line = existingLines[i];
-      ctx.moveTo(line.startX, line.startY);
-      ctx.lineTo(line.endX, line.endY);
-
-    }
-
-    for (i in existingPenPaths) {
-      drawPenPath(existingPenPaths[i]);
-    }
-
-    ctx.stroke();
 
 
     if (isDrawing) {
-      ctx.strokeStyle = "darkred";
-      ctx.lineWidth = 3;
+      ctx.strokeStyle = color;
+      ctx.lineWidth = lineWidth;
       ctx.beginPath();
       ctx.moveTo(startX, startY);
       ctx.lineTo(mouseX, mouseY); 
@@ -79,23 +61,23 @@ function draw() {
     ctx.setTransform(1, 0, 0, 1, 0, 0);
   }
 
-  if (reflect) {
-    for (var j = 0; j < numPoints; j++) {
+  for (i in paths) {
+
+    const path = paths[i];
+    const ROTATE2 = PI / (path.numAxii / 2);
+  
+    for (var j = 0; j < path.numAxii; j++) {
 
       ctx.translate( canvasWidth / 2, canvasHeight / 2 );
-      ctx.rotate(ROTATE * j);
+      ctx.rotate(ROTATE2 * j);
       ctx.translate( -canvasWidth / 2, -canvasHeight / 2 );
   
       ctx.beginPath();
 
-      for (var i = 0; i < existingLines.length; ++i) {
-          var line = existingLines[i];
-          ctx.moveTo(  - line.startX  + canvasWidth, line.startY);
-          ctx.lineTo ( - line.endX + canvasWidth,   line.endY );
-      }
-
-      for (i in existingPenPaths) {
-        drawPenPath(existingPenPaths[i], true);
+      drawPenPath(path);
+      
+      if (path.reflect) {
+        drawPenPath(path, true);
       }
   
       ctx.stroke();
@@ -106,10 +88,16 @@ function draw() {
   }
 }
 
+
+function drawMode() {
+  return document.getElementById('line').checked ? 'line' : 'pen'
+}
+
+
 function onmousedown(e) {
   if (hasLoaded && e.button === 0) {
 
-    if (drawMode == 'line') {
+    if (drawMode() == 'line') {
       if (!isDrawing) {
         startX = e.clientX - bounds.left;
         startY = e.clientY - bounds.top;
@@ -120,7 +108,7 @@ function onmousedown(e) {
   }
   mouseDown = true;
 
-  if (drawMode == 'pen') {
+  if (drawMode() == 'pen') {
     findxy('down', e);
   }
 
@@ -130,13 +118,23 @@ function onmousedown(e) {
 function onmouseup(e) {
   if (hasLoaded && e.button === 0) {
     if (isDrawing) {
-      existingLines.push({
-        startX: startX,
-        startY: startY,
-        endX: mouseX,
-        endY: mouseY
+
+      var path = {points: []};
+      path.points.push({
+        x: startX,
+        y: startY
+      });
+      path.points.push({
+    
+        x: mouseX,
+        y: mouseY
       });
 
+      path.color = document.getElementById('color').value;
+      path.reflect = reflect;
+      path.numAxii = numPoints;
+
+      paths.push(path);
       isDrawing = false;
     }
 
@@ -144,7 +142,7 @@ function onmouseup(e) {
   }
   mouseDown = false;
 
-  if (drawMode == 'pen') {
+  if (drawMode() == 'pen') {
     findxy('up', e);
   }
 }
@@ -155,11 +153,11 @@ function onmousemove(e) {
     mouseY = e.clientY - bounds.top;
 
 
-    if (drawMode == 'line' && isDrawing) {
+    if (drawMode() == 'line' && isDrawing) {
       draw();
     }
 
-    if (drawMode == 'pen') {
+    if (drawMode() == 'pen') {
       findxy('move', e);
       draw();
     }
@@ -170,45 +168,11 @@ function onKeypress(e) {
 
   // delete last line
   if (e.code == 'KeyU') {
-    existingLines.pop();
+    paths.pop();
     draw();
   }
 
 }
-
-window.onload = function() {
-  canvas = document.getElementById("canvas");
-  canvas.width = canvasWidth;
-  canvas.height = canvasHeight;
-  canvas.onmousedown = onmousedown;
-  canvas.onmouseup = onmouseup;
-  canvas.onmousemove = onmousemove;
-
-  canvas.addEventListener("mouseout", function (e) {
-    findxy('out', e)
-  }, false);
-  
-  document.addEventListener("keypress", onKeypress);
-
-  document.getElementById('points').value = numPoints;
-  document.getElementById('reflect').checked = reflect;
-  document.getElementById('line').checked = true;
-
-  document.getElementById('points').onchange = function(e) {
-    numPoints = e.target.value;
-  }
-  document.getElementById('reflect').onchange = function(e) {
-    reflect = e.target.checked;
-  }
-
-  bounds = canvas.getBoundingClientRect();
-  ctx = canvas.getContext("2d");
-  hasLoaded = true;
-
-  draw();
-}
-
-
 
 flag = false,
 prevX = 0,
@@ -218,12 +182,15 @@ currY = 0;
 
 
 function mark() {
-  var currentPenPath = existingPenPaths[existingPenPaths.length - 1];
+  var currentPenPath = paths[paths.length - 1];
   currentPenPath.points.push({
     
     x: currX,
     y: currY
   });
+
+  currentPenPath.reflect = reflect;
+  currentPenPath.numAxii = numPoints;
 }
 
 function drawPenPath(path, reflect = false) {
@@ -251,7 +218,7 @@ function drawPenPath(path, reflect = false) {
 
 function findxy(res, e) {
   if (res == 'down') {
-      existingPenPaths.push({points:[], color: document.getElementById('color').value});
+      paths.push({points:[], color: document.getElementById('color').value});
       prevX = currX;
       prevY = currY;
       currX = e.clientX - canvas.offsetLeft;
@@ -277,6 +244,35 @@ function findxy(res, e) {
   }
 }
 
-function drawMode() {
-  return 
+window.onload = function() {
+  canvas = document.getElementById("canvas");
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
+  canvas.onmousedown = onmousedown;
+  canvas.onmouseup = onmouseup;
+  canvas.onmousemove = onmousemove;
+
+  canvas.addEventListener("mouseout", function (e) {
+    findxy('out', e)
+  }, false);
+  
+  document.addEventListener("keypress", onKeypress);
+
+  document.getElementById('points').value = numPoints;
+  document.getElementById('reflect').checked = reflect;
+  document.getElementById('line').checked = true;
+  document.getElementById('color').value = color
+
+  document.getElementById('points').onchange = function(e) {
+    numPoints = e.target.value;
+  }
+  document.getElementById('reflect').onchange = function(e) {
+    reflect = e.target.checked;
+  }
+
+  bounds = canvas.getBoundingClientRect();
+  ctx = canvas.getContext("2d");
+  hasLoaded = true;
+
+  draw();
 }
